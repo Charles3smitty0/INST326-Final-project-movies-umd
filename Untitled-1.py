58de5b788f679657cabcb24971ffe6668ef24e11
gseyoum <getasey@gmail.com>
2022-04-17 19:44:35 -0400

    save

---
+++ Untitled-1.py
@@ -0,0 +1,28 @@
+"""
+Final Project 
+names: Getahun Seyoum, Yamlak Shimelis, Charlie Smith
+Date: 04/17/2022
+Course: INST 326
+"""
+import pandas as pd
+
+"""We will need to find a database cv file of various movies
+ we can use in our project to recommend movies"""
+
+
+"""The user will input a movie title they've enjoyed which will then 
+be used to recommend another or a list of movies they will also enjoy"""
+
+
+"""The find_director function will take the name of the director for
+for the film that was inputted by the user and match it with other films
+in the dataset the director has also directed"""
+def find_director(d):
+
+
+
+
+
+"""We will then biuld the recommendation system based 
+on similar actors/directors/genres/etc.""" 
+
