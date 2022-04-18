91404511fe1cc02309a8df26308a172df118cd8c
gseyoum <getasey@gmail.com>
2022-04-17 23:11:25 -0400

    save

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,23 +4,21 @@ names: Getahun Seyoum, Yamlak Shimelis, Charlie Smith
 Date: 04/17/2022
 Course: INST 326
 """
+from asyncore import read
 import pandas as pd
 
 """We will need to find a database cv file of various movies
  we can use in our project to recommend movies"""
-
+movies=pd.read_csv
 
 """The user will input a movie title they've enjoyed which will then 
 be used to recommend another or a list of movies they will also enjoy"""
-
+def movie_seen(f):
 
 """The find_director function will take the name of the director for
 for the film that was inputted by the user and match it with other films
 in the dataset the director has also directed"""
-def find_director(d):
-
-
-
+def find_director(x):
 
 
 """We will then biuld the recommendation system based 
